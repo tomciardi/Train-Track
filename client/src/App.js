e5119493b31676c62c29e1da20d3203faa2e43c2
tomciardi <72100170+tomciardi@users.noreply.github.com>
2021-05-06 15:25:38 -0400

    cleaned up css and routing

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,12 @@ import NavigationBar from "./Routing/NavigationBar"
 function App() {
   return (
     <div className="App">
-        <NavigationBar />
-        <Routes />
+      <link rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+        crossOrigin="anonymous"/>
+      <NavigationBar />
+      <Routes />
     </div>
   );
 }
--- client/src/Routing/NavigationBar.js
+++ client/src/Routing/NavigationBar.js
@@ -1,15 +1,18 @@
-import {Nav, Navbar} from 'react-bootstrap'
+import { Nav, Navbar } from 'react-bootstrap'
 
 const NavigationBar = () => {
     return (
         <>
+
             <Navbar expand="sm" bg="dark" variant="dark">
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="container-fluid">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/tutorial">Tutorial</Nav.Link>
+                <Navbar.Brand href="/">Train-Track</Navbar.Brand>
+                <Nav className="container-fluid">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/tutorial">Tutorial</Nav.Link>
+                    <Nav className = "ml-auto">
+                        <Nav.Link href="/">Sign Up</Nav.Link>
                     </Nav>
-                </Navbar.Collapse>
+                </Nav>
             </Navbar>
         </>
     );
