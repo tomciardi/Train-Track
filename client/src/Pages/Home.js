8c2c3841c9e595c137e3345a1ff2754a5904b500
tomciardi <72100170+tomciardi@users.noreply.github.com>
2021-05-06 16:52:06 -0400

    table query

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import { useState } from 'react'
+import { Card, Container, Dropdown, DropdownButton, Table } from 'react-bootstrap'
+import Axios from "axios";
 
 export default function Home() {
+
+    const [companyList, setCompanyList] = useState([]);
+    const [queried, setQueried] = useState(false)
+
+    const getCompanies = () => {
+        Axios.get("http://localhost:3001/companies").then((response) => {
+            setCompanyList(response.data)
+            console.log(companyList)
+        });
+        setQueried(true)
+    };
+
+    const QueryTable = () => {
+        return (
+            <Table striped condensed hover variant="dark">
+            <thead>
+                <RenderTable />
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </Table>
+        )
+    }
+
+    function RenderTable(list) {
+        const keys = Object.keys(list)
+
+        console.log(keys)
+        let keys_headers = []
+
+        for(let i = 0; i < keys.length; i++) {
+            keys_headers.push(<th>{keys[0]}</th>)
+        }
+
+        console.log(keys_headers)
+        return (
+            <tr>
+                { keys_headers }
+            </tr>
+        )
+    }
+
     return (
-        <div>
-            Home
-        </div>
+        <>
+        <Container className="d-flex align-tems-center justify-content-center" style={{ minHeight: "100vh" }}>
+                <div className="w-100" style={{ maxWidth: "800px" }}>
+                    <Card>
+                        <Card.Body>
+                            <h2 className="text-center mb-4">Choose Table</h2>
+                            <DropdownButton title="Select Table">
+                                <Dropdown.Item onClick={getCompanies}>Company</Dropdown.Item>
+                            </DropdownButton>
+                        </Card.Body>
+                    </Card>
+                    {queried && <QueryTable />}
+                </div>
+            </Container>
+        </>
     )
 }
--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
   user: "root",
   host: "localhost",
   password: "@cwruTC17",
-  database: "employeesystem",
+  database: "train_trackdb",
 });
 
 app.post("/create", (req, res) => {
@@ -69,6 +69,16 @@ app.delete("/delete/:id", (req, res) => {
     }
   });
 });
+
+app.get("/companies", (req, res) => {
+    db.query("SELECT * FROM company", (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
 
 app.listen(3001, () => {
   console.log("Yey, your server is running on port 3001");
